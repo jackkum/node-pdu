c6e2716f802770a07c36ac0d1f47c7e4fa6f7e59
Sergey Ryazanov <ryazanov.s.a@gmail.com>
2018-11-08 01:36:10 +0300

    tests: add GSM 7 bit encoder/decoder tests
    Add encoder/decoder invocation code and a few tests for them.

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 var pdu = require('node-pdu');
+var Helper = pdu.getModule('PDU/Helper');
 
 var testNum;
 var cntTotal = 0;
@@ -64,6 +65,66 @@ function verifyResultPdu(logPrefix, res, expRes, err)
     }
 
     return true;
+}
+
+var sevenBitEncodingTests = [
+    {
+        name: 'Lowercase letters',
+        text: 'abcdefghijklmnopqrstuvwxyz',
+        code: '61F1985C369FD169F59ADD76BFE171F99C5EB7DFF1793D',
+    }, {
+        name: 'Uppercase letters',
+        text: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        code: '41E19058341E9149E592D9743EA151E9945AB55EB1592D',
+    }, {
+        name: 'Digits',
+        text: '0123456789',
+        code: 'B0986C46ABD96EB81C',
+    }, {
+        name: '1 symbol', text: 'a', code: '61',
+    }, {
+        name: '2 symbols', text: 'ab', code: '6131',
+    }, {
+        name: '3 symbols', text: 'abc', code: '61F118',
+    }, {
+        name: '4 symbols', text: 'abcd', code: '61F1980C',
+    }, {
+        name: '5 symbols', text: 'abcde', code: '61F1985C06',
+    }, {
+        name: '6 symbols', text: 'abcdef', code: '61F1985C3603',
+    }, {
+        name: '7 symbols', text: 'abcdefg', code: '61F1985C369F01',
+    }, {
+        name: '8 symbols', text: 'abcdefgh', code: '61F1985C369FD1',
+    }, {
+        name: '9 symbols', text: 'abcdefghi', code: '61F1985C369FD169',
+    }
+];
+
+testNum = 0;
+for (let test of sevenBitEncodingTests) {
+    var logPrefix = makeLogPrefix('7BitEncoding', ++testNum, test.name);
+    var out = undefined;
+    var passed = true;
+
+    cntTotal++;
+
+    out = Helper.encode7Bit(test.text);
+    if (out[1] != test.code) {
+        passed = false;
+        console.log(logPrefix + 'fail: encoder error (text: "' + test.text + '", expecting: "' + test.code + '", got "' + out[1] + '")');
+    }
+
+    out = Helper.decode7Bit(test.code);
+    if (out != test.text) {
+        passed = false;
+        console.log(logPrefix + 'fail: decoder error (code: "' + test.code + '", expecting: "' + test.text + '", got "' + out + '")');
+    }
+
+    if (passed) {
+        cntOk++;
+        console.log(logPrefix + 'Ok');
+    }
 }
 
 var parserTests = [
