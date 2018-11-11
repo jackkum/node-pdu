jackkum/node-pdu

commit 39e0a64ffb84cff83442993128cca99c68842cdf

SCTS: make _getDateTime() time zone aware
Earlier implementation implicitly convert UTC timestamp to local time
and append it with zero time zone offset. Fix this puzzle by outputing
date and time in earlier specified time zone and explicitly indicate
this time zone in the time zone offset subfield.

PDU/SCTS.js
@@ -82,15 +82,25 @@ SCTS.prototype.getTzOff = function()
  */
 SCTS.prototype._getDateTime = function()
 {
-    var dt = new Date(this.getTime() * 1000);
+    /**
+     * Since the JS can not output time in the specified TimeZone we first
+     * manually shift the UTC timestamp onto tzOffset and then use
+     * getUTC{Year,Month,etc.}() methods to get a Year, Month, etc.
+     */
+    var tz = this.getTzOff();
+    var dt = new Date((this.getTime() + tz * 60) * 1000);
+
+    tz = Math.floor(tz / 15);   /* To quarters of an hour */
     return sprintf(
-        '%02d%02d%02d%02d%02d%02d00', 
-        dt.getYear(),
-        dt.getMonth() + 1,
-        dt.getDate(),
-        dt.getHours(),
-        dt.getMinutes(),
-        dt.getSeconds()
+        '%02d%02d%02d%02d%02d%02d%02X',
+        dt.getUTCFullYear() % 100,
+        dt.getUTCMonth() + 1,
+        dt.getUTCDate(),
+        dt.getUTCHours(),
+        dt.getUTCMinutes(),
+        dt.getUTCSeconds(),
+        Math.floor(Math.abs(tz / 10)) * 16 + tz % 10 +
+        (tz < 0 ? 0x80 : 0x00)
     );
 };
 

commit 9f612402ac605ca43cd8179ddb8618fefa67d51e

SCTS: fix negative offset encoding
The remainder of the division can be negative if dividend is negative,
which leads to corruption of the result of the Time Zone offset encoding
if the offset is negative.

Fix this issue by taking the absolute value of the Time Zone offset and
using this value everyware in the encoder of SCTS.

Fixes: 39e0a64 ("SCTS: make _getDateTime() time zone aware")

PDU/SCTS.js
@@ -117,10 +117,10 @@ SCTS.prototype._getDateTime = function()
      * manually shift the UTC timestamp onto tzOffset and then use
      * getUTC{Year,Month,etc.}() methods to get a Year, Month, etc.
      */
-    var tz = this.getTzOff();
+    var tz = this.getTzOff(), tzAbs = 0;
     var dt = new Date((this.getTime() + tz * 60) * 1000);
 
-    tz = Math.floor(tz / 15);   /* To quarters of an hour */
+    tzAbs = Math.floor(Math.abs(tz) / 15);    /* To quarters of an hour */
     return sprintf(
         '%02d%02d%02d%02d%02d%02d%02X',
         dt.getUTCFullYear() % 100,
@@ -129,7 +129,7 @@ SCTS.prototype._getDateTime = function()
         dt.getUTCHours(),
         dt.getUTCMinutes(),
         dt.getUTCSeconds(),
-        Math.floor(Math.abs(tz / 10)) * 16 + tz % 10 +
+        Math.floor(tzAbs / 10) * 16 + tzAbs % 10 +
         (tz < 0 ? 0x80 : 0x00)
     );
 };